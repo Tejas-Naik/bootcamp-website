6e813d0064be0f811224bcc5e9894822715f7a43
Tejas-Naik <rntejas2005@gmail.com>
2024-09-23 21:48:08 +0530

    downgraded react

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+// import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom';
+
 import App from './App.jsx'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+ReactDOM.render(
+<StrictMode>
+<App />
+</StrictMode>
+, document.getElementById('root'));
